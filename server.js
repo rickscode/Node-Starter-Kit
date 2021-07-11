1d26470a2bffe122237dd558df6097b62b8f9972
“Ricky <grickyh@gmail.com>
2021-07-11 11:51:05 +0100

    Handling and routing

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+
 // import express in to file
 const express = require('express');
 // initialise express server app
 const app = express();
+// Handler function to send message back to client with send() method
+app.get("/", (req, res) => {
+    console.log(req);
+    res.send("Yay Daisy!");
+});
+// Routing example with new end point
+app.get("/Daisy", (req, res) => {
+    console.log(req);
+    res.send("Yay Daisy you served a new response!");
+});
 // listen for requests and execute callback 
 app.listen(3000, () => {
     console.log("Server is listening on port 3000. Ready to accept requests!");
